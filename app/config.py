e9135cb63f60b9dd977cbc93c0fcd528092b19c6
voidwest <mineofshadow@gmail.com>
2026-04-11 12:00:44 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,8 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data/docs"
 DB_DIR = BASE_DIR / "vector_db"
 
-LLM_MODEL = "modeldiscussed"
-LLM_BASE_URL = "urlillfindsomehow"
+LLM_MODEL = "smollm2:1.7b"
+LLM_BASE_URL = "http://localhost:11434"
 TEMPERATURE = 0.1  #
 
 
@@ -16,3 +16,4 @@ DEVICE = "cpu"
 VECTOR_DB_PATH = str(DB_DIR / "chroma_store")
 CHUNK_SIZE = 512
 CHUNK_OVERLAP = 64
+#
