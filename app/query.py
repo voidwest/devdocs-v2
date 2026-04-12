73e46bea3966fdd501dc61681394ca9461cc8838
voidwest <mineofshadow@gmail.com>
2026-04-12 13:58:58 +0300

    Update query.py

--- app/query.py
+++ app/query.py
@@ -1,7 +1,8 @@
-import config
 import chromadb
-from chromadb.utils import embedding_functions
+import config
 import requests
+from chromadb.utils import embedding_functions
+
 
 def get_context(query, n_result=3):
-client = chromadb.PersistentClient(path=config.VECTOR_DB_PATH)
+    client = chromadb.PersistentClient(path=config.VECTOR_DB_PATH)
