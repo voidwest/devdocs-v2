0072da2e6deda9cde84dc08603b383f4246b57f0
voidwest <mineofshadow@gmail.com>
2026-04-12 14:19:47 +0300

    Update query.py

--- app/query.py
+++ app/query.py
@@ -6,3 +6,7 @@ from chromadb.utils import embedding_functions
 
 def get_context(query, n_result=3):
     client = chromadb.PersistentClient(path=config.VECTOR_DB_PATH)
+    emb_fn = embedding_functions.SentenceTransformerEmbeddingFunction(
+        model_name=config.EMBEDDING_MODEL_NAME
+    )
+    collection = client.get_collection(name="devdocs", embedding_function=emb_fn)
