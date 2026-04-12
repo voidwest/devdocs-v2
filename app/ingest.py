e6d8a0229f128cd31703f6abe58440ed604761b9
voidwest <mineofshadow@gmail.com>
2026-04-12 12:21:48 +0300

    Update ingest.py

--- app/ingest.py
+++ app/ingest.py
@@ -19,9 +19,11 @@ def get_text(path):
 
 
 def chunk_text(text, chunk_size, overlap):
+    words = text.split()
     chunks = []
-    for i in range(0, len(text), chunk_size - overlap):
-        chunks.append(text[i : i + chunk_size])
+    for i in range(0, len(words), chunk_size - overlap):
+        chunk = " ".join(words[i : i + chunk_size])
+        chunks.append(chunk)
     return chunks
 
 
