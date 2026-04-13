987fe6365100659bb778ce468219883e9878f920
voidwest <mineofshadow@gmail.com>
2026-04-13 14:15:12 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,7 @@
 import os
 
 import config
+from fastapi import FastAPI
 from ingest import run_ingest
+from pydantic import BaseModel
 from query import query_docs
-
-
-def main():
-    if not os.path.exists(config.VECTOR_DB_PATH):
-        run_ingest()
-
-    print("test msg")
-    while True:
-        user_input = input("msg: ")
-        if user_input.lower() in ["exit", "quit"]:
-            break
-
-        response = query_docs(user_input)
-        print(f"\nAI: {response}\n")
